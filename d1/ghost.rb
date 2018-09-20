963a6e0da0e43382a3c790348294ec7f024082e5
Darren <deid@appacademy.io>
2018-09-20 14:26:33 -0700

    Game class

--- d1/ghost.rb
+++ d1/ghost.rb
@@ -9,12 +9,62 @@ end
 class Game
 
   def initialize(*players)
-    players.each {|p| @play << p}
     @fragment = ""
+    @play = []
+    players.each {|p| @play << p}
+    @current_player = @play[0]
+    @next_player = @play[1]
+    @previous_player = nil
     @dictionary = import("./data/dictionary.txt")
   end
 
-  def test
-    p dictionary.length
+  def play_round
+
   end
+
+  def current_player
+    @current_player
+  end
+
+  def previous_player
+    @previous_player
+  end
+
+  def next_player!
+    @next_player
+  end
+
+  def take_turn(player)
+
+  end
+
+  def valid_play?(string)
+
+  end
+
+  def fragment
+    @fragment
+  end
+
+  def play
+    @play
+  end
+
+  def dictionary
+    @dictionary
+  end
+end
+
+class Player
+  def initialize(name)
+    @name = name
+  end
+
+  def guess
+
+  end
+
+  def alert_invalid_guess
+
+  end 
 end
