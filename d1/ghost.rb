37d33de40ea6af07f3829014d12b2ae6f1313158
Darren <deid@appacademy.io>
2018-09-20 16:45:46 -0700

    default

--- d1/ghost.rb
+++ d1/ghost.rb
@@ -12,46 +12,43 @@ class Game
     @fragment = ""
     @play = []
     players.each {|p| @play << p}
-    @current_player = @play[0]
-    @next_player = @play[1]
+    @current_player = Player.new(play[0])
+    @next_player = Player.new(play[1])
     @previous_player = nil
     @dictionary = import("./data/dictionary.txt")
   end
 
   def play_round
-
-  end
-
-  def current_player
-    @current_player
-  end
-
-  def previous_player
-    @previous_player
-  end
-
-  def next_player!
-    @next_player
+    
   end
 
   def take_turn(player)
-
+    p "Take your turn, #{player.name}"
+    entry = gets.chomp
   end
 
   def valid_play?(string)
+    if string.length == 1
 
   end
 
-  def fragment
+  def fragment(x = @fragment)
+    @fragment = x
     @fragment
   end
 
-  def play
-    @play
+  def current_player(x = @current_player)
+    @current_player = x
+    @current_player
   end
 
-  def dictionary
-    @dictionary
+  def next_player
+    @next_player, @current_player = @current_player, @next_player
+  end
+
+  def previous_player (x = @previous_player)
+    @previous_player = x
+    @previous_player
   end
 end
 
@@ -67,4 +64,12 @@ class Player
   def alert_invalid_guess
 
   end 
+
+  def name
+    @name
+  end
 end
+
+
+rob = Game.new("Bob", "Ross") 
+rob.play_round
